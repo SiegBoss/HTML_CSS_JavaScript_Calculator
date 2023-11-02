d1924b0205cebf01b7be6fb6211e8dcbdaee55a5
SiegBoss <146117384+SiegBoss@users.noreply.github.com>
2023-11-02 13:22:43 -0600

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -7,42 +7,39 @@ var hidden_operation = "";
 //Funcion | Function
 function agregar(valor) {
 
+    //Si detecta AC, borra todo | If detect AC, delete all
     if(valor == 'AC') {
-        see_operation = "";
-        
-    } else if(valor == '=') {
+        hidden_operation = "";
+       
+    } 
+    //Si detecta =, evalua la operacion | If detect =, evaluate the operation
+    else if(valor == '=') {
         
         try {
-            see_operation = eval(see_operation).toString();
+            hidden_operation = eval(hidden_operation).toString();
+
         } catch(e) {
-            see_operation = "Error";
+
+            hidden_operation = "Error al realizar la operacion";
         }
-    }
 
-    see_operation += valor;
-    document.getElementById('contacto-div__input').value = see_operation;
+    }
+    //Si detecta Pi, agrega el valor de Pi | If detect Pi, add the value of Pi
+    else if(valor == 'Pi') {
+        let pi = Math.PI.toString();
+        let pi_rounded = pi.substring(0, 4);
+        hidden_operation += pi_rounded;
+    }
+    //Si detecta e, agrega el valor de e | If detect e, add the value of e
+    else if(valor == 'e') {
+        let e = Math.E.toString();
+        let e_rounded = e.substring(0, 5);
+        hidden_operation += e_rounded;
+    }
+    //Si detecta cualquier otro valor, lo agrega | If detect any other value, add it
+    else{
+
+        hidden_operation += valor;
+    }
+    document.getElementById('contacto-div__input').value = hidden_operation;
 }
-
-
- /*
-    switch(valor) {
-        case 'AC':
-            input = "";
-            break;
-        case '=':
-            try {
-                input = eval(input).toString();
-            } catch(e) {
-                input = "Error";
-            }
-            break;
-        case 'Pi':
-            input += Math.PI.toString();
-            break;
-        case 'e':
-            input += Math.E.toString();
-            break;
-        default:
-            input += valor;
-    }
-    */
