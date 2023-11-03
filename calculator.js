5ca9fecf4fe4d729256284e79c89107aa1e5fec0
SiegBoss <146117384+SiegBoss@users.noreply.github.com>
2023-11-02 18:35:00 -0600

    Update
    HTML CSS JavaScript

--- calculator.js
+++ calculator.js
@@ -8,38 +8,63 @@ var hidden_operation = "";
 function agregar(valor) {
 
     //Si detecta AC, borra todo | If detect AC, delete all
-    if(valor == 'AC') {
+    if(valor == 'C') {
         hidden_operation = "";
+        see_operation = "";
        
     } 
+    //Si detecta A, borra el ultimo valor | If detect CE, delete the last value
+    else if(valor == 'A') {
+        hidden_operation = hidden_operation.substring(0, hidden_operation.length - 1);
+        see_operation = see_operation.substring(0, see_operation.length - 1);
+    }
     //Si detecta =, evalua la operacion | If detect =, evaluate the operation
     else if(valor == '=') {
         
         try {
             hidden_operation = eval(hidden_operation).toString();
+            see_operation = hidden_operation;
 
         } catch(e) {
 
-            hidden_operation = "Error al realizar la operacion";
+            see_operation = "Error al realizar la operacion";
         }
-
     }
-    //Si detecta Pi, agrega el valor de Pi | If detect Pi, add the value of Pi
-    else if(valor == 'Pi') {
+    //Si detecta P, agrega el valor de pi | If detect P, add the value of pi
+    else if(valor == 'P') {
         let pi = Math.PI.toString();
         let pi_rounded = pi.substring(0, 4);
         hidden_operation += pi_rounded;
+        see_operation += pi_rounded;
     }
     //Si detecta e, agrega el valor de e | If detect e, add the value of e
     else if(valor == 'e') {
         let e = Math.E.toString();
         let e_rounded = e.substring(0, 5);
         hidden_operation += e_rounded;
+        see_operation += e_rounded;
+    }
+    //Si detecta * muestra X | If detect *, show X
+    else if (valor == '*'){
+        hidden_operation += '*';
+        see_operation += 'X';   
+    }
+    //Si detecta ** muestra ^ | If detect **, show ^
+    else if (valor == '**'){
+        hidden_operation += '**';
+        see_operation += '^';   
+    }
+    //Si detecta R muestra √ | If detect R, show √
+    else if (valor == 'R'){
+        hidden_operation += 'Math.sqrt';
+        see_operation += '√';   
     }
     //Si detecta cualquier otro valor, lo agrega | If detect any other value, add it
     else{
-
         hidden_operation += valor;
+        see_operation += valor;
     }
-    document.getElementById('contacto-div__input').value = hidden_operation;
+
+    //Muestra la operacion | Show the operation
+    document.getElementById('contacto-div__div-input').value = see_operation;
 }
